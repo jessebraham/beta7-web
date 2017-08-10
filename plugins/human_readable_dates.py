0496b1d5a4272eac63cc5933e3a477868f7e60bf
Jesse Braham <jbraham92@gmail.com>
2017-08-09 22:41:57 -0700

    Human readable date plugin added

---
+++ plugins/human_readable_dates.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+human_readable_dates.py
+
+Jesse Braham <jesse@beta7.io>
+August 2017
+
+A simple module to convert timestamps to a human-readable format. Uses the
+Arrow module.
+
+https://arrow.readthedocs.io/en/latest/
+'''
+
+import arrow
+
+from pelican import signals
+
+
+def format_time(timestamp):
+    '''
+    Parse the provided timestamp into an Arrow object, then return the
+    formatted string.
+
+    MMMM - long month      (August)
+    Do   - day with suffix (1st)
+    YYYY - full year       (2017)
+    '''
+
+    return arrow.get(timestamp).format('MMMM Do, YYYY')
+
+def add_filter(pelican):
+    '''
+    Add the filter to the global list of filters in the Pelican Environment
+    object.
+    '''
+
+    pelican.env.filters.update({'human_readable': format_time})
+
+def register():
+    '''
+    Register the filter to run on the 'generator_init' signal.
+    '''
+
+    signals.generator_init.connect(add_filter)
