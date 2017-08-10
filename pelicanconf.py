6aec54d3a42651f0bdbd265a29a26e70b186ff9f
Jesse Braham <jbraham92@gmail.com>
2017-08-10 16:53:18 -0700

    pelicanconf and requirements updated

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 '''
-Development Configuration
+Development Configuration for Pelican
 '''
 
 
 # #############################################################################
-#  Metadata
+#  Site Information & Metadata
 # #############################################################################
 
 SITENAME = 'Beta7'
@@ -14,34 +14,51 @@ SITEURL = ''
 
 AUTHOR = 'Jesse Braham'
 COPYRIGHT = 'Jesse Braham'
-DESCRIPTION = ''
+
 SITE_HEADER_TITLE = 'Go away'
-GITHUB_USER = 'jessebraham'
+DESCRIPTION = 'Populate me you bitch'
 
+DEFAULT_LANG = 'en'
 TIMEZONE = 'America/Vancouver'
-DEFAULT_LANG = 'English'
-HTML_LANG = 'en'
-
-MENU_ITEMS = (('Home', ''),
-              ('Archives', 'archives.html'),
-              ('Categories', 'categories.html'),)
+GITHUB_URL = 'https://github.com/jessebraham'
 
 
 # #############################################################################
-#  Configuration
+#  Site Configuration
 # #############################################################################
-
-PATH = 'content'
-THEME = 'beta7-theme'
 
 DEFAULT_PAGINATION = 10
 SUMMARY_MAX_LENGTH = 32
 
 RELATIVE_URLS = True
 
+DISPLAY_PAGES_ON_MENU = True
+
 # We will use whatever folder a post is stored in as its category to keep
 # things simple and organized.
 USE_FOLDER_AS_CATEGORY = True
+
+# Items to display in the main navigation, with their respective URLs.
+MENU_ITEMS = (('Home', ''),
+              ('Archives', 'archives.html'),
+              ('Categories', 'categories.html'),)
+
+
+# #############################################################################
+#  File Path Configuration
+# #############################################################################
+
+PATH = './content'
+THEME = './beta7-theme'
+
+# Copy any extra required files to the output directory.
+EXTRA_PATH_METADATA = {'extra/favicon.ico': {'path': 'favicon.ico'},
+                       'extra/robots.txt': {'path': 'robots.txt'},}
+
+# Do not create the 'author.html', 'authors.html' and 'tag.html' pages.
+AUTHOR_SAVE_AS = False
+AUTHORS_SAVE_AS = False
+TAG_SAVE_AS = False
 
 # Articles should be stored in folders with the name of the category, and the
 # URL should match the pattern
@@ -53,12 +70,11 @@ ARTICLE_URL = '{category}/{slug}.html'
 PAGE_SAVE_AS = '{slug}.html'
 PAGE_URL = '{slug}.html'
 
-# Do not create the 'author.html', 'authors.html' and 'tag.html' pages.
-AUTHOR_SAVE_AS = False
-AUTHORS_SAVE_AS = False
-TAG_SAVE_AS = False
 
-# Feed generation is usually not desired when developing
+# #############################################################################
+#  Feed Configuration
+# #############################################################################
+
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
@@ -81,6 +97,10 @@ PLUGINS = [
     'sitemap',
 ]
 
+SITEMAP = {
+    'format': 'xml',
+}
+
 MARKDOWN = {
     'extension_configs': {
         'extensions.luminous': {},
@@ -88,8 +108,4 @@ MARKDOWN = {
         'markdown.extensions.tables': {},
         'markdown.extensions.toc': {},
     },
-}
-
-SITEMAP = {
-    'format': 'xml',
 }
