61e87d81f7440fdc27af639882fdadb2dbffcb74
Jesse Braham <jesse@beta7.io>
2018-02-05 21:09:38 -0800

    ...guess I needed that after all

--- publishconf.py
+++ publishconf.py
@@ -8,6 +8,14 @@
     :license: MIT, see LICENSE for more details.
 '''
 
+# Insert the current directory into the system path so that pelicanconf can be
+# imported successfully.
+import os
+import sys
+
+sys.path.insert(0, os.getcwd())
+
+
 # Import all configuration from our Development Configuration as a starting
 # point, since we will be overriding a few values.
 from pelicanconf import *  # NOQA
